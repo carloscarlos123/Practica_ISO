ac3835e2a5682d65bb3e26eccf598fac3e795324
carloscarlos123 <cruedaguzman@gmail.com>
2024-10-21 12:31:55 +0200

    Añadimos menú para elegir suma o resta

--- mi_calculadora.sh
+++ mi_calculadora.sh
@@ -3,4 +3,12 @@ clear
 
 source funciones_math.sh
 
-suma $1 $2
+operacion=$1
+
+if [[ "$operacion" == "s" ]]; then
+	suma $2 $3
+elif [[ "$operacion" == "r" ]]; then
+	resta $2 $3
+else
+	echo "Solo está disponible la suma(s) y la resta(r)."
+fi
